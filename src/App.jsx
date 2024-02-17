daa7cab81d35897b2109a3a0c96ab0075357d2ba
A_Botir <shirokirutsuga>
2024-02-17 23:30:56 +0500

    commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+function App() {
+  fetch("https://min-api.cryptocompare.com/data/price?fsym=<Price>&tsyms=USD&api_key=<b93a092d590136167ecf788d7c6ed97daa7095ed7facfbe20d28562d36e3195b&gt")
+  return (
+    <>
+    </>
+  )
+}
+
+export default App
---
+++ src/assets/components/Form.jsx
@@ -0,0 +1,5 @@
+function Form() {
+    <form className="m-auto">
+        
+    </form>
+}
